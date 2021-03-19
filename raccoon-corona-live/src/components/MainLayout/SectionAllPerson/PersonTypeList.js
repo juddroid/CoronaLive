3837970d794ed7e4dbf5ea955bbaa71a60e79f68
Raccoon <nobllizdc@gmail.com>
2021-03-20 03:05:50 +0900

    [Fix] 약간의 css 조정

--- raccoon-corona-live/src/components/MainLayout/SectionAllPerson/PersonTypeList.js
+++ raccoon-corona-live/src/components/MainLayout/SectionAllPerson/PersonTypeList.js
@@ -22,24 +22,28 @@ function PersonTypeList() {
         number: caseNumber,
         change: caseChange,
         color: 'rgb(235, 83, 116)',
+        background: 'rgba(235, 83, 116, 0.082)',
       },
       {
         name: '사망자',
         number: deathsNumber,
         change: deathsChange,
         color: 'rgb(136, 136, 136)',
+        background: 'rgb(39, 43, 56)',
       },
       {
         name: '완치자',
         number: recoveredNumber,
         change: recoveredChange,
         color: 'rgb(23, 138, 23)',
+        background: 'rgba(22, 171, 22, 0.16)',
       },
       {
         name: '검사자',
         number: testsNumber,
         change: testsChange,
         color: 'rgb(86, 115, 235)',
+        background: 'rgba(86, 115, 235, 0.082)',
       },
     ]);
   }
@@ -50,12 +54,13 @@ function PersonTypeList() {
 
   return (
     <div className="Person__Type__List flex-row center">
-      {data.map(({ name, number, change, color }, idx) => (
+      {data.map(({ name, number, change, color, background }, idx) => (
         <PersonTypeBox //
           type={name}
           number={number}
           change={change}
           color={color}
+          background={background}
           key={idx}
         />
       ))}
@@ -63,8 +68,8 @@ function PersonTypeList() {
   );
 }
 
-function PersonTypeBox({ type, number, change, color }) {
-  const style = { color };
+function PersonTypeBox({ type, number, change, color, background }) {
+  const style = { color, background };
   return (
     <div className="Person__Type__Box flex-column flex center">
       <div className="Person__Type flex-row flex">{type}</div>
